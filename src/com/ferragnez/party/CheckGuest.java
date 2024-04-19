fd351ce0371226a5cd6a99e0e8ea367be7981f49
StefaniaVitali <stefy.egypt@hotmail.it>
2024-04-19 21:53:30 +0200

    Add: invitati array

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,21 @@
 package com.ferragnez.party;
 
 public class CheckGuest {
+	
+	/* nome repo: java-festa-ferragnez
+State lavorando col servizio di sicurezza dei Ferragnez e il vostro compito è di assicurarvi 
+che accedano alla festa solo gli invitati presenti sulla lista
+Nel programma bisogna: creare e inizializzare l’array contenente i nomi degli invitati 
+chiedere all’utente come si chiama e  verificare che il nome sia presente nella lista; 
+lasciarlo entrare o rispedirlo cortesemente da dove è venuto 
+Lista invitati: Dua Lipa, Paris Hilton, Manuel Agnelli, J-Ax, Francesco Totti, Ilary Blasi, Bebe Vio, Luis, Pardis Zarei, Martina Maccherone, Rachel Zeilic */
+	
+	public static void main (String [] args ) {
+		
+		//inizializzare l'array di invitati
+		
+		String [] invitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti","Ilary Blasi", "Bebe Vio","Luis","Pardis Zarei", "Martina Maccherone","Rachel Zeilic" };
+		
+	}
 
 }
